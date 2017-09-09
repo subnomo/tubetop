e2b10bb81f1e96b75a00eba26f23420aa7429eae
Alex Taylor <alex@alext.xyz>
2017-09-08 21:09:31 -0700

    WIP

--- src/renderer/components/NavBar/index.tsx
+++ src/renderer/components/NavBar/index.tsx
@@ -1,25 +1,8 @@
 import * as React from 'react';
-import styled from 'styled-components';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
-import Typography from 'material-ui/Typography';
-import IconButton from 'material-ui/IconButton';
 import MenuIcon from 'material-ui-icons/Menu';
-
-// Styled components
-
-const Root = styled.div`
-  width: 100%;
-`;
-
-const MenuButton = styled(IconButton)`
-  margin-left: 12px;
-  margin-right: 20px;
-`;
-
-const FlexTypo = styled(Typography)`
-  flex: 1;
-`;
+import { Root, MenuButton, FlexTypo } from './styles';
 
 interface IProps extends React.Props<NavBar> {
   title: string;
