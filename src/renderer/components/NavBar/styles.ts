19ac200b99390efec8bd5bb62b2699ae5be9fb9d
Alex Taylor <alex@subnomo.me>
2017-09-08 21:09:31 -0700

    WIP

---
+++ src/renderer/components/NavBar/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+import IconButton from 'material-ui/IconButton';
+import Typography from 'material-ui/Typography';
+
+// Styled components
+
+export const Root = styled.div`
+  width: 100%;
+`;
+
+export const MenuButton = styled(IconButton)`
+  margin-left: 12px;
+  margin-right: 20px;
+`;
+
+export const FlexTypo = styled(Typography)`
+  flex: 1;
+`;
