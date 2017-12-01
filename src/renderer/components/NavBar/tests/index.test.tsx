f8af5efc0fb395c70242b94d756c88ac425e14c8
Alex Taylor <alex@subnomo.me>
2017-12-01 05:16:28 -0800

    Add enzyme

---
+++ src/renderer/components/NavBar/tests/index.test.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { shallow } from 'enzyme';
+
+import NavBar from '../index';
+
+describe('<NavBar />', () => {
+  it('should render title', () => {
+    const renderedComponent = shallow(<NavBar title="TestTitle" />);
+    expect(renderedComponent.contains('TestTitle')).toBe(true);
+  });
+
+  it('should toggle drawer', () => {
+    const renderedComponent = shallow(<NavBar title="title" />).instance();
+    expect(renderedComponent.state.drawerOpen).toBe(false);
+    renderedComponent.toggleDrawer();
+    expect(renderedComponent.state.drawerOpen).toBe(true);
+  });
+});
